f9ee29f69fc9c747405bdf86522b2084635174ec
ranitadelamatrix <gastontrejo9099@hotmail.com>
2024-04-09 20:15:50 -0300

    "consumo api"

--- login.js
+++ login.js
@@ -1,19 +1,12 @@
-
-
-
-
-
-document.getElementById('registrationForm').addEventListener('submit', async function(event) {
-    event.preventDefault();
+function cargar(){
 
     // Obtener los valores del formulario
-    const dni = document.getElementById('dni').value;
+    const dni = document.getElementById('dni').value
     const nombre = document.getElementById('nombre').value;
     const apellido = document.getElementById('apellido').value;
+    const direccion = document.getElementById('direccion').value;
     const localidad = document.getElementById('localidad').value;
-    const direccion = document.getElementById('direccion');
     const telefono = document.getElementById('telefono').value;
-    const fecha_nacimiento = document.getElementById( 'fecha_nacimiento' ).value;
     const contraseña = document.getElementById('contraseña').value;
 
     // Construir objeto con los datos del formulario
@@ -21,38 +14,30 @@ document.getElementById('registrationForm').addEventListener('submit', async fun
         dni: dni,
         nombre: nombre,
         apellido: apellido,
-        localidad: localidad,
         direccion: direccion,
+        localidad: localidad,
         telefono: telefono,
-        fecha_nacimiento: fecha_nacimiento,
-        constraseña: contraseña
+        contraseña: contraseña
 
     };
 
     try {
         // Enviar los datos al servidor
-        const response = await fetch('http://localhost:3000/', {
+        const response = fetch('http://localhost:3000/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(datos)
         });
+        alert("bienvenido " +nombre+ " tu usuario se cargo correctamente")
 
-        if (!response.ok) {
-            console.error("el error es", error)
-            throw new Error('Error al enviar los datos');
+            console.log("carga correcta")
         
         }
-
-        // Limpiar el formulario después de enviar los datos
-        this.reset();
-
-        // Mostrar mensaje de éxito
-        alert('Integrante agregado exitosamente');
-    } catch (error) {
-        // Mostrar mensaje de error si ocurre algún problema
-        console.error('Error al enviar los datos:', error);
-        alert('Hubo un error al agregar el integrante');
-    }
-});
+    
+    catch(error){
+            console.log(error)
+        }
+        
+    };
